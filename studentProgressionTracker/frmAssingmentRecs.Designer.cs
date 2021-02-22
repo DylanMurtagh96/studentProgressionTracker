d05be5b977e4b8e758fb76447723b89e8c5f8a7b
kincaidandrew <kincaidandrew2000@outlook.com>
2021-02-22 10:11:25 +0000

    Just working on the assignments file

--- studentProgressionTracker/frmAssingmentRecs.Designer.cs
+++ studentProgressionTracker/frmAssingmentRecs.Designer.cs
@@ -29,12 +29,168 @@ namespace studentProgressionTracker
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.assignmentIDTbx = new System.Windows.Forms.TextBox();
+            this.courseIDTbx = new System.Windows.Forms.TextBox();
+            this.moduleIDTbx = new System.Windows.Forms.TextBox();
+            this.assignmentsGbx = new System.Windows.Forms.GroupBox();
+            this.taskValueTbx = new System.Windows.Forms.TextBox();
+            this.taskNameTbx = new System.Windows.Forms.TextBox();
+            this.taskOutlineTbx = new System.Windows.Forms.TextBox();
+            this.assignmentIDLbl = new System.Windows.Forms.Label();
+            this.courseIDLbl = new System.Windows.Forms.Label();
+            this.moduleIDLbl = new System.Windows.Forms.Label();
+            this.taskNameLbl = new System.Windows.Forms.Label();
+            this.taskValueLbl = new System.Windows.Forms.Label();
+            this.taskOutlineLbl = new System.Windows.Forms.Label();
+            this.assignmentsGbx.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // assignmentIDTbx
+            // 
+            this.assignmentIDTbx.Location = new System.Drawing.Point(202, 32);
+            this.assignmentIDTbx.Name = "assignmentIDTbx";
+            this.assignmentIDTbx.Size = new System.Drawing.Size(100, 22);
+            this.assignmentIDTbx.TabIndex = 0;
+            // 
+            // courseIDTbx
+            // 
+            this.courseIDTbx.Location = new System.Drawing.Point(202, 74);
+            this.courseIDTbx.Name = "courseIDTbx";
+            this.courseIDTbx.Size = new System.Drawing.Size(100, 22);
+            this.courseIDTbx.TabIndex = 1;
+            // 
+            // moduleIDTbx
+            // 
+            this.moduleIDTbx.Location = new System.Drawing.Point(202, 114);
+            this.moduleIDTbx.Name = "moduleIDTbx";
+            this.moduleIDTbx.Size = new System.Drawing.Size(100, 22);
+            this.moduleIDTbx.TabIndex = 2;
+            // 
+            // assignmentsGbx
+            // 
+            this.assignmentsGbx.Controls.Add(this.moduleIDLbl);
+            this.assignmentsGbx.Controls.Add(this.courseIDLbl);
+            this.assignmentsGbx.Controls.Add(this.assignmentIDLbl);
+            this.assignmentsGbx.Controls.Add(this.assignmentIDTbx);
+            this.assignmentsGbx.Controls.Add(this.moduleIDTbx);
+            this.assignmentsGbx.Controls.Add(this.courseIDTbx);
+            this.assignmentsGbx.Location = new System.Drawing.Point(67, 77);
+            this.assignmentsGbx.Name = "assignmentsGbx";
+            this.assignmentsGbx.Size = new System.Drawing.Size(329, 163);
+            this.assignmentsGbx.TabIndex = 3;
+            this.assignmentsGbx.TabStop = false;
+            // 
+            // taskValueTbx
+            // 
+            this.taskValueTbx.Location = new System.Drawing.Point(561, 151);
+            this.taskValueTbx.Name = "taskValueTbx";
+            this.taskValueTbx.Size = new System.Drawing.Size(100, 22);
+            this.taskValueTbx.TabIndex = 4;
+            // 
+            // taskNameTbx
+            // 
+            this.taskNameTbx.Location = new System.Drawing.Point(561, 105);
+            this.taskNameTbx.Name = "taskNameTbx";
+            this.taskNameTbx.Size = new System.Drawing.Size(100, 22);
+            this.taskNameTbx.TabIndex = 5;
+            // 
+            // taskOutlineTbx
+            // 
+            this.taskOutlineTbx.Location = new System.Drawing.Point(67, 287);
+            this.taskOutlineTbx.Multiline = true;
+            this.taskOutlineTbx.Name = "taskOutlineTbx";
+            this.taskOutlineTbx.Size = new System.Drawing.Size(608, 172);
+            this.taskOutlineTbx.TabIndex = 6;
+            // 
+            // assignmentIDLbl
+            // 
+            this.assignmentIDLbl.AutoSize = true;
+            this.assignmentIDLbl.Location = new System.Drawing.Point(29, 33);
+            this.assignmentIDLbl.Name = "assignmentIDLbl";
+            this.assignmentIDLbl.Size = new System.Drawing.Size(98, 17);
+            this.assignmentIDLbl.TabIndex = 3;
+            this.assignmentIDLbl.Text = "Assignment ID";
+            // 
+            // courseIDLbl
+            // 
+            this.courseIDLbl.AutoSize = true;
+            this.courseIDLbl.Location = new System.Drawing.Point(29, 79);
+            this.courseIDLbl.Name = "courseIDLbl";
+            this.courseIDLbl.Size = new System.Drawing.Size(70, 17);
+            this.courseIDLbl.TabIndex = 4;
+            this.courseIDLbl.Text = "Course ID";
+            // 
+            // moduleIDLbl
+            // 
+            this.moduleIDLbl.AutoSize = true;
+            this.moduleIDLbl.Location = new System.Drawing.Point(29, 119);
+            this.moduleIDLbl.Name = "moduleIDLbl";
+            this.moduleIDLbl.Size = new System.Drawing.Size(71, 17);
+            this.moduleIDLbl.TabIndex = 5;
+            this.moduleIDLbl.Text = "Module ID";
+            // 
+            // taskNameLbl
+            // 
+            this.taskNameLbl.AutoSize = true;
+            this.taskNameLbl.Location = new System.Drawing.Point(441, 109);
+            this.taskNameLbl.Name = "taskNameLbl";
+            this.taskNameLbl.Size = new System.Drawing.Size(80, 17);
+            this.taskNameLbl.TabIndex = 7;
+            this.taskNameLbl.Text = "Task Name";
+            // 
+            // taskValueLbl
+            // 
+            this.taskValueLbl.AutoSize = true;
+            this.taskValueLbl.Location = new System.Drawing.Point(442, 151);
+            this.taskValueLbl.Name = "taskValueLbl";
+            this.taskValueLbl.Size = new System.Drawing.Size(79, 17);
+            this.taskValueLbl.TabIndex = 8;
+            this.taskValueLbl.Text = "Task Value";
+            // 
+            // taskOutlineLbl
+            // 
+            this.taskOutlineLbl.AutoSize = true;
+            this.taskOutlineLbl.Location = new System.Drawing.Point(96, 252);
+            this.taskOutlineLbl.Name = "taskOutlineLbl";
+            this.taskOutlineLbl.Size = new System.Drawing.Size(88, 17);
+            this.taskOutlineLbl.TabIndex = 9;
+            this.taskOutlineLbl.Text = "Task Outline";
+            // 
+            // frmAssingmentRecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAssingmentRecs";
+            this.ClientSize = new System.Drawing.Size(848, 548);
+            this.Controls.Add(this.taskOutlineLbl);
+            this.Controls.Add(this.taskValueLbl);
+            this.Controls.Add(this.taskNameLbl);
+            this.Controls.Add(this.taskOutlineTbx);
+            this.Controls.Add(this.taskNameTbx);
+            this.Controls.Add(this.taskValueTbx);
+            this.Controls.Add(this.assignmentsGbx);
+            this.Name = "frmAssingmentRecs";
+            this.Text = "Assignments informtation";
+            this.assignmentsGbx.ResumeLayout(false);
+            this.assignmentsGbx.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox assignmentIDTbx;
+        private System.Windows.Forms.TextBox courseIDTbx;
+        private System.Windows.Forms.TextBox moduleIDTbx;
+        private System.Windows.Forms.GroupBox assignmentsGbx;
+        private System.Windows.Forms.Label moduleIDLbl;
+        private System.Windows.Forms.Label courseIDLbl;
+        private System.Windows.Forms.Label assignmentIDLbl;
+        private System.Windows.Forms.TextBox taskValueTbx;
+        private System.Windows.Forms.TextBox taskNameTbx;
+        private System.Windows.Forms.TextBox taskOutlineTbx;
+        private System.Windows.Forms.Label taskNameLbl;
+        private System.Windows.Forms.Label taskValueLbl;
+        private System.Windows.Forms.Label taskOutlineLbl;
     }
 }
