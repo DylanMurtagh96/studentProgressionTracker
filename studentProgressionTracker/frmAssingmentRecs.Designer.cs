5a0eed48197535916c6d6e1e3d22486345514b43
kincaidandrew <kincaidandrew2000@outlook.com>
2021-02-22 11:11:38 +0000

    got the assingment form working

--- studentProgressionTracker/frmAssingmentRecs.Designer.cs
+++ studentProgressionTracker/frmAssingmentRecs.Designer.cs
@@ -29,28 +29,33 @@ namespace studentProgressionTracker
         /// </summary>
         private void InitializeComponent()
         {
-            this.assignmentIDTbx = new System.Windows.Forms.TextBox();
+            this.taskIDTbx = new System.Windows.Forms.TextBox();
             this.courseIDTbx = new System.Windows.Forms.TextBox();
             this.moduleIDTbx = new System.Windows.Forms.TextBox();
             this.assignmentsGbx = new System.Windows.Forms.GroupBox();
             this.taskValueTbx = new System.Windows.Forms.TextBox();
             this.taskNameTbx = new System.Windows.Forms.TextBox();
             this.taskOutlineTbx = new System.Windows.Forms.TextBox();
-            this.assignmentIDLbl = new System.Windows.Forms.Label();
+            this.taskIDLbl = new System.Windows.Forms.Label();
             this.courseIDLbl = new System.Windows.Forms.Label();
             this.moduleIDLbl = new System.Windows.Forms.Label();
             this.taskNameLbl = new System.Windows.Forms.Label();
             this.taskValueLbl = new System.Windows.Forms.Label();
             this.taskOutlineLbl = new System.Windows.Forms.Label();
+            this.recordTrackerTbx = new System.Windows.Forms.TextBox();
+            this.firstBtn = new System.Windows.Forms.Button();
+            this.previousBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.lastBtn = new System.Windows.Forms.Button();
             this.assignmentsGbx.SuspendLayout();
             this.SuspendLayout();
             // 
-            // assignmentIDTbx
+            // taskIDTbx
             // 
-            this.assignmentIDTbx.Location = new System.Drawing.Point(202, 32);
-            this.assignmentIDTbx.Name = "assignmentIDTbx";
-            this.assignmentIDTbx.Size = new System.Drawing.Size(100, 22);
-            this.assignmentIDTbx.TabIndex = 0;
+            this.taskIDTbx.Location = new System.Drawing.Point(202, 32);
+            this.taskIDTbx.Name = "taskIDTbx";
+            this.taskIDTbx.Size = new System.Drawing.Size(100, 22);
+            this.taskIDTbx.TabIndex = 0;
             // 
             // courseIDTbx
             // 
@@ -70,11 +75,11 @@ namespace studentProgressionTracker
             // 
             this.assignmentsGbx.Controls.Add(this.moduleIDLbl);
             this.assignmentsGbx.Controls.Add(this.courseIDLbl);
-            this.assignmentsGbx.Controls.Add(this.assignmentIDLbl);
-            this.assignmentsGbx.Controls.Add(this.assignmentIDTbx);
+            this.assignmentsGbx.Controls.Add(this.taskIDLbl);
+            this.assignmentsGbx.Controls.Add(this.taskIDTbx);
             this.assignmentsGbx.Controls.Add(this.moduleIDTbx);
             this.assignmentsGbx.Controls.Add(this.courseIDTbx);
-            this.assignmentsGbx.Location = new System.Drawing.Point(67, 77);
+            this.assignmentsGbx.Location = new System.Drawing.Point(67, 131);
             this.assignmentsGbx.Name = "assignmentsGbx";
             this.assignmentsGbx.Size = new System.Drawing.Size(329, 163);
             this.assignmentsGbx.TabIndex = 3;
@@ -82,34 +87,34 @@ namespace studentProgressionTracker
             // 
             // taskValueTbx
             // 
-            this.taskValueTbx.Location = new System.Drawing.Point(561, 151);
+            this.taskValueTbx.Location = new System.Drawing.Point(561, 210);
             this.taskValueTbx.Name = "taskValueTbx";
             this.taskValueTbx.Size = new System.Drawing.Size(100, 22);
             this.taskValueTbx.TabIndex = 4;
             // 
             // taskNameTbx
             // 
-            this.taskNameTbx.Location = new System.Drawing.Point(561, 105);
+            this.taskNameTbx.Location = new System.Drawing.Point(561, 163);
             this.taskNameTbx.Name = "taskNameTbx";
             this.taskNameTbx.Size = new System.Drawing.Size(100, 22);
             this.taskNameTbx.TabIndex = 5;
             // 
             // taskOutlineTbx
             // 
-            this.taskOutlineTbx.Location = new System.Drawing.Point(67, 287);
+            this.taskOutlineTbx.Location = new System.Drawing.Point(67, 346);
             this.taskOutlineTbx.Multiline = true;
             this.taskOutlineTbx.Name = "taskOutlineTbx";
             this.taskOutlineTbx.Size = new System.Drawing.Size(608, 172);
             this.taskOutlineTbx.TabIndex = 6;
             // 
-            // assignmentIDLbl
+            // taskIDLbl
             // 
-            this.assignmentIDLbl.AutoSize = true;
-            this.assignmentIDLbl.Location = new System.Drawing.Point(29, 33);
-            this.assignmentIDLbl.Name = "assignmentIDLbl";
-            this.assignmentIDLbl.Size = new System.Drawing.Size(98, 17);
-            this.assignmentIDLbl.TabIndex = 3;
-            this.assignmentIDLbl.Text = "Assignment ID";
+            this.taskIDLbl.AutoSize = true;
+            this.taskIDLbl.Location = new System.Drawing.Point(29, 33);
+            this.taskIDLbl.Name = "taskIDLbl";
+            this.taskIDLbl.Size = new System.Drawing.Size(56, 17);
+            this.taskIDLbl.TabIndex = 3;
+            this.taskIDLbl.Text = "Task ID";
             // 
             // courseIDLbl
             // 
@@ -132,7 +137,7 @@ namespace studentProgressionTracker
             // taskNameLbl
             // 
             this.taskNameLbl.AutoSize = true;
-            this.taskNameLbl.Location = new System.Drawing.Point(441, 109);
+            this.taskNameLbl.Location = new System.Drawing.Point(440, 166);
             this.taskNameLbl.Name = "taskNameLbl";
             this.taskNameLbl.Size = new System.Drawing.Size(80, 17);
             this.taskNameLbl.TabIndex = 7;
@@ -141,7 +146,7 @@ namespace studentProgressionTracker
             // taskValueLbl
             // 
             this.taskValueLbl.AutoSize = true;
-            this.taskValueLbl.Location = new System.Drawing.Point(442, 151);
+            this.taskValueLbl.Location = new System.Drawing.Point(441, 210);
             this.taskValueLbl.Name = "taskValueLbl";
             this.taskValueLbl.Size = new System.Drawing.Size(79, 17);
             this.taskValueLbl.TabIndex = 8;
@@ -150,17 +155,69 @@ namespace studentProgressionTracker
             // taskOutlineLbl
             // 
             this.taskOutlineLbl.AutoSize = true;
-            this.taskOutlineLbl.Location = new System.Drawing.Point(96, 252);
+            this.taskOutlineLbl.Location = new System.Drawing.Point(78, 315);
             this.taskOutlineLbl.Name = "taskOutlineLbl";
             this.taskOutlineLbl.Size = new System.Drawing.Size(88, 17);
             this.taskOutlineLbl.TabIndex = 9;
             this.taskOutlineLbl.Text = "Task Outline";
             // 
+            // recordTrackerTbx
+            // 
+            this.recordTrackerTbx.Location = new System.Drawing.Point(322, 541);
+            this.recordTrackerTbx.Name = "recordTrackerTbx";
+            this.recordTrackerTbx.Size = new System.Drawing.Size(97, 22);
+            this.recordTrackerTbx.TabIndex = 10;
+            // 
+            // firstBtn
+            // 
+            this.firstBtn.Location = new System.Drawing.Point(70, 532);
+            this.firstBtn.Name = "firstBtn";
+            this.firstBtn.Size = new System.Drawing.Size(105, 40);
+            this.firstBtn.TabIndex = 11;
+            this.firstBtn.Text = "&First";
+            this.firstBtn.UseVisualStyleBackColor = true;
+            this.firstBtn.Click += new System.EventHandler(this.firstBtn_Click_1);
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Location = new System.Drawing.Point(193, 532);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(105, 40);
+            this.previousBtn.TabIndex = 12;
+            this.previousBtn.Text = "&Previous";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            this.previousBtn.Click += new System.EventHandler(this.previousBtn_Click_1);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(443, 532);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(105, 40);
+            this.nextBtn.TabIndex = 13;
+            this.nextBtn.Text = "&Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click_1);
+            // 
+            // lastBtn
+            // 
+            this.lastBtn.Location = new System.Drawing.Point(570, 532);
+            this.lastBtn.Name = "lastBtn";
+            this.lastBtn.Size = new System.Drawing.Size(105, 40);
+            this.lastBtn.TabIndex = 14;
+            this.lastBtn.Text = "&Last";
+            this.lastBtn.UseVisualStyleBackColor = true;
+            this.lastBtn.Click += new System.EventHandler(this.lastBtn_Click_1);
+            // 
             // frmAssingmentRecs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 548);
+            this.ClientSize = new System.Drawing.Size(848, 595);
+            this.Controls.Add(this.lastBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.previousBtn);
+            this.Controls.Add(this.firstBtn);
+            this.Controls.Add(this.recordTrackerTbx);
             this.Controls.Add(this.taskOutlineLbl);
             this.Controls.Add(this.taskValueLbl);
             this.Controls.Add(this.taskNameLbl);
@@ -170,6 +227,7 @@ namespace studentProgressionTracker
             this.Controls.Add(this.assignmentsGbx);
             this.Name = "frmAssingmentRecs";
             this.Text = "Assignments informtation";
+            this.Load += new System.EventHandler(this.frmAssingmentRecs_Load);
             this.assignmentsGbx.ResumeLayout(false);
             this.assignmentsGbx.PerformLayout();
             this.ResumeLayout(false);
@@ -179,18 +237,23 @@ namespace studentProgressionTracker
 
         #endregion
 
-        private System.Windows.Forms.TextBox assignmentIDTbx;
+        private System.Windows.Forms.TextBox taskIDTbx;
         private System.Windows.Forms.TextBox courseIDTbx;
         private System.Windows.Forms.TextBox moduleIDTbx;
         private System.Windows.Forms.GroupBox assignmentsGbx;
         private System.Windows.Forms.Label moduleIDLbl;
         private System.Windows.Forms.Label courseIDLbl;
-        private System.Windows.Forms.Label assignmentIDLbl;
+        private System.Windows.Forms.Label taskIDLbl;
         private System.Windows.Forms.TextBox taskValueTbx;
         private System.Windows.Forms.TextBox taskNameTbx;
         private System.Windows.Forms.TextBox taskOutlineTbx;
         private System.Windows.Forms.Label taskNameLbl;
         private System.Windows.Forms.Label taskValueLbl;
         private System.Windows.Forms.Label taskOutlineLbl;
+        private System.Windows.Forms.TextBox recordTrackerTbx;
+        private System.Windows.Forms.Button firstBtn;
+        private System.Windows.Forms.Button previousBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button lastBtn;
     }
 }
--- studentProgressionTracker/frmAssingmentRecs.cs
+++ studentProgressionTracker/frmAssingmentRecs.cs
@@ -7,14 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+
 
 namespace studentProgressionTracker
 {
     public partial class frmAssingmentRecs : Form
     {
+        OleDbConnection conn;
+        OleDbCommand taskCommand;
+        OleDbDataAdapter taskAdapter;
+        DataTable taskTable;
+        CurrencyManager taskManager;
+        OleDbCommandBuilder taskCommBuilder;
+
+        bool dbError = false;
+
         public frmAssingmentRecs()
         {
             InitializeComponent();
+        }
+
+        ///This code is run each time the application is opened it connects to the database and
+        //binds the data to the text boxes
+        private void frmAssingmentRecs_Load(object sender, EventArgs e)
+        {
+            var connString = @"Provider = Microsoft.ACE.OLEDB.12.0;" +
+                            @"Data Source = ..\..\..\courseModuleDB.accdb;";
+            //+ @" Jet OLEDB:Database Password = killester";
+
+            conn = new OleDbConnection(connString);
+            conn.Open();
+            recordTrackerTbx.Text = conn.State.ToString();
+
+            //create command object and pass SQL command and connection object 
+            taskCommand = new OleDbCommand("Select * from assignmentsTbl", conn);
+            //create a data table
+            taskAdapter = new OleDbDataAdapter();
+            taskAdapter.SelectCommand = taskCommand;
+            //create table
+            taskTable = new DataTable();
+            //fill the data table witht the info returned from the query using the data adapter
+            taskAdapter.Fill(taskTable);
+
+            //dataBinding
+            taskIDTbx.DataBindings.Add("Text", taskTable, "taskID");
+            courseIDTbx.DataBindings.Add("Text", taskTable, "courseID");
+            moduleIDTbx.DataBindings.Add("Text", taskTable, "moduleID");
+            //weekBeginningTbx.DataBindings.Add("Text", moduleTable, "weekBeginning");
+            //durationTbx.DataBindings.Add("Text", moduleTable, "moduleDuration");
+            taskNameTbx.DataBindings.Add("Text", taskTable, "taskName");
+            taskValueTbx.DataBindings.Add("Text", taskTable, "taskValue");
+            taskOutlineTbx.DataBindings.Add("Text", taskTable, "taskOutline");
+
+            //dateRegisteredTbx.DataBindings.Add(new System.Windows.Forms.Binding("Text", carTable, "DateRegistered", true, DataSourceUpdateMode.OnPropertyChanged, null, "dd/MM/yyyy"));
+            //rentalPerDayTbx.DataBindings.Add(new System.Windows.Forms.Binding("Text", carTable, "RentalPerDay", true, DataSourceUpdateMode.OnPropertyChanged, null, "c2"));
+            //availableCbx.DataBindings.Add("CheckState", carTable, "Available", true, DataSourceUpdateMode.OnPropertyChanged, CheckState.Unchecked);
+
+            taskManager = (CurrencyManager)BindingContext[taskTable];
+            recordCount();
+        }
+
+        //Code keeps record count updated
+        private void recordCount()
+        {
+            recordTrackerTbx.Text = (taskManager.Position + 1) + " of " + taskManager.Count;
+        }
+
+        private void firstBtn_Click_1(object sender, EventArgs e)
+        {
+            if (taskManager.Position != 0)
+            {
+                taskManager.Position = 0;
+                recordCount();
+            }
+            else
+            {
+                MessageBox.Show("You are already at the first record", "First Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void previousBtn_Click_1(object sender, EventArgs e)
+        {
+            if (taskManager.Position != 0)
+            {
+                taskManager.Position--;
+                recordCount();
+            }
+            else
+            {
+                MessageBox.Show("You are at the first record", "First Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void nextBtn_Click_1(object sender, EventArgs e)
+        {
+            if (taskManager.Position != taskManager.Count - 1)
+            {
+                taskManager.Position++;
+                recordCount();
+            }
+            else
+            {
+                MessageBox.Show("You are at the last record", "Last Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void lastBtn_Click_1(object sender, EventArgs e)
+        {
+            if (taskManager.Position != taskManager.Count - 1)
+            {
+                taskManager.Position = taskManager.Count - 1;
+                recordCount();
+            }
+            else
+            {
+                MessageBox.Show("You are already at the last record", "Last Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
