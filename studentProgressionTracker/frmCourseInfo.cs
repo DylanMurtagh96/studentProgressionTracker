82d8f08eadef71433592a59a811a676db7878298
DylanMurtagh96 <dylanmurtzz@gmail.com>
2021-02-22 10:27:43 +0000

    Course form now displays coirse information

--- studentProgressionTracker/frmCourseInfo.cs
+++ studentProgressionTracker/frmCourseInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,13 @@ namespace studentProgressionTracker
 {
     public partial class frmCourseInfo : Form
     {
+        OleDbConnection conn;
+        OleDbCommand courseCommand;
+        OleDbDataAdapter courseAdapter;
+        DataTable courseTable;
+        CurrencyManager courseManager;
+        OleDbCommandBuilder courseCommBuilder;
+
         public frmCourseInfo()
         {
             InitializeComponent();
@@ -19,7 +27,30 @@ namespace studentProgressionTracker
 
         private void frmCourseInfo_Load(object sender, EventArgs e)
         {
+            var connString = @"Provider = Microsoft.ACE.OLEDB.12.0;" +
+                            @"Data Source = ..\..\..\courseModuleDB.accdb;";
+            
 
+            conn = new OleDbConnection(connString);
+            conn.Open();
+           
+            //create command object and pass SQL command and connection object 
+            courseCommand = new OleDbCommand("Select * from course", conn);
+            //create a data table
+            courseAdapter = new OleDbDataAdapter();
+            courseAdapter.SelectCommand = courseCommand;
+            //create table
+            courseTable = new DataTable();
+            //fill the data table witht the info returned from the query using the data adapter
+            courseAdapter.Fill(courseTable);
+
+            //dataBinding
+            txtCourseName.DataBindings.Add("Text", courseTable, "courseName");
+            txtID.DataBindings.Add("Text", courseTable, "courseID");
+            txtTutor.DataBindings.Add("Text", courseTable, "tutor");
+            txtAddress.DataBindings.Add("Text", courseTable, "address");
+           
+            
         }
 
         private void picboxHome_Click(object sender, EventArgs e)
